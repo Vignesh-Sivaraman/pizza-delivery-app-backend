cf5f40edb5eadf3742cfcf514f306803641946f5
Sirius-18 <97686497+Vignesh-Sivaraman@users.noreply.github.com>
2022-10-02 15:35:08 +0530

    sign in,login, email verifiy completed

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ import { MongoClient } from "mongodb";
 const URL = process.env.DB;
 import nodemailer from "nodemailer";
 
+// initializing express
 const app = express();
 
 // MidleWare
+
 app.use(express.json());
+
 //cors
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -20,6 +24,7 @@ app.use(
 );
 
 // Establishing connection to database
+
 const createConnection = async () => {
   const client = new MongoClient(URL);
   await client.connect();
@@ -27,8 +32,6 @@ const createConnection = async () => {
   return client;
 };
 const client = await createConnection();
-
-// mail verification
 
 // nodemailer transporter module
 
@@ -43,6 +46,8 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// mail sender
+
 const verifymail = async (email, subject, text) => {
   try {
     await transporter.sendMail({
@@ -51,34 +56,25 @@ const verifymail = async (email, subject, text) => {
       subject: subject,
       text: text,
     });
-    console.log("email sent successfully");
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
+// Web Server Check
+
 app.get("/", (req, res) => {
-  res.json({ message: "Success" });
+  res.json({ message: "Reporting for duty" });
 });
 
-// app.post("/register", async (req, res) => {
-//   try {
-//     let user = client.db("pizzaDB").collection("Registration");
-//     let salt = await bcrypt.genSalt(10);
-//     let hash = await bcrypt.hash(req.body.password, salt);
-//     req.body.password = hash;
-//     let final = await user.insertOne(req.body);
-//     res.json({ message: "User successfully registered" });
-//   } catch (err) {
-//     console.log(err);
-//     res.json(err);
-//   }
-// });
+// Admin/user register
 
-// register
 app.post("/register", async (req, res) => {
   try {
     let user = client.db("pizzaDB").collection("Registration");
+    let userexists = await user.findOne({ email: req.body.email });
+    if (userexists)
+      return res.status(401).json({ message: "Email Already exists" });
     let salt = await bcrypt.genSalt(Number(process.env.SALT));
     let hash = await bcrypt.hash(req.body.password, salt);
     req.body.password = hash;
@@ -102,14 +98,18 @@ app.post("/register", async (req, res) => {
       .toArray();
 
     const verifyurl = `${process.env.BASE_URL}/${getentry.hashID}/verify/${getentry.token}`;
-    await verifymail(req.body.email, "Verify Email", verifyurl);
+    await verifymail(
+      req.body.email,
+      "Verify Email",
+      `Hi I am form Pizza Lair,\n Please Click the below link to verify Your email \n ${verifyurl}`
+    );
     res.status(200).json({ message: "An email sent to your mail id" });
   } catch (err) {
     res.status(500).json({ message: `something went wrong; ${err}` });
   }
 });
 
-// verifying email
+// verifying email for registration
 
 app.get("/register/:id/verify/:token", async (req, res) => {
   try {
@@ -125,11 +125,17 @@ app.get("/register/:id/verify/:token", async (req, res) => {
       { _id: token._id },
       { $set: { verified: true, hashID: "", token: "" } }
     );
+    await user.findOneAndUpdate(
+      { _id: token._id },
+      { $unset: { hashID: "", token: "" } }
+    );
     res.status(200).json({ message: "Email verified Successfully" });
   } catch (err) {
     res.status(500).json({ message: `something went wrong; ${err}` });
   }
 });
+
+// Admin/user Login
 
 app.post("/login", async (req, res) => {
   try {
@@ -152,9 +158,8 @@ app.post("/login", async (req, res) => {
           expiresIn: "2 days",
         });
         res.status(200).json({ token, type });
-        // res.json({ message: "logged in successfully" });
       } else {
-        res.status(404).json({
+        res.status(401).json({
           message:
             "password is wrong or email is not verified \n Kindly Check your Password or verify your Email",
         });
@@ -166,6 +171,107 @@ app.post("/login", async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
+
+// forgot password registration
+
+app.post("/forpass", async (req, res) => {
+  try {
+    let user = client.db("pizzaDB").collection("Registration");
+
+    let userExists = await user.findOne({ email: req.body.email });
+    if (!userExists)
+      return res.status(401).json({ message: "Invalid Email ID" });
+    let salt = await bcrypt.genSalt(Number(process.env.SALT));
+    let nameHash = await bcrypt.hash(req.body.email, salt);
+    let verifySalt = await bcrypt.genSalt(Number(process.env.SALT));
+    let passID = jwt.sign({ _id: nameHash }, process.env.SECRETPASSKEY, {
+      expiresIn: "2 days",
+    });
+    let passtoken = jwt.sign({ _id: verifySalt }, process.env.SECRETEMAILKEY, {
+      expiresIn: "2 days",
+    });
+
+    let insertedData = await user.findOneAndUpdate(
+      { email: req.body.email },
+      {
+        $set: {
+          passID: passID,
+          passtoken: passtoken,
+          passstatus: false,
+        },
+      }
+    );
+    let [getentry] = await user.find({ _id: insertedData.value._id }).toArray();
+
+    const verifyurl = `${process.env.BASE_URL}/forpass/${getentry.passID}/verify/${getentry.passtoken}`;
+    await verifymail(
+      req.body.email,
+      "Verify Email to reset password",
+      `Hi i am form Pizza Lair, \n Please Click the below link to reset Your password \n ${verifyurl}`
+    );
+    res.status(200).json({ message: "An email sent to your mail id" });
+  } catch (err) {
+    res.status(500).json({ message: `something went wrong; ${err}` });
+  }
+});
+
+// verifying forgot password
+
+app.get("/forpass/:id/verify/:token", async (req, res) => {
+  try {
+    let user = client.db("pizzaDB").collection("Registration");
+    const dataToVerify = await user.findOne({ passID: req.params.id });
+    if (!dataToVerify) return res.status(400).json({ message: "invalid link" });
+    const token = await user.findOne({
+      _id: dataToVerify._id,
+      passtoken: req.params.token,
+    });
+    if (!token) return res.status(400).json({ message: "invalid link" });
+    await user.findOneAndUpdate(
+      { _id: token._id },
+      { $set: { passstatus: true, passID: "", passtoken: "" } }
+    );
+    res.status(200).json({ message: "Email verified Successfully" });
+  } catch (err) {
+    res.status(500).json({ message: `something went wrong; ${err}` });
+  }
+});
+
+// reset password
+
+app.post("/resetpass", async (req, res) => {
+  try {
+    //getting the data from the db for the sent email
+    let user = client.db("pizzaDB").collection("Registration");
+
+    let userExists = await user.findOne({ email: req.body.email });
+    if (!userExists)
+      return res.status(401).json({ message: "Invalid Email ID" });
+    let salt = await bcrypt.genSalt(Number(process.env.SALT));
+    let hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+    if (userExists.passstatus) {
+      await user.findOneAndUpdate(
+        { email: req.body.email },
+        { $set: { password: req.body.password } }
+      );
+      await user.findOneAndUpdate(
+        { email: req.body.email },
+        { $unset: { passstatus: "", passID: "", passtoken: "" } }
+      );
+      res.status(200).json({ message: "Password changed successfully" });
+    } else {
+      res.status(401).json({
+        message:
+          "Email is not verified for password reset \n Kindly verify your Email",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+// port listen
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("server listening on port 3001");
